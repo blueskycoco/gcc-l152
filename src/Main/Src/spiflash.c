b460e93fd31dd1d59ccf72ceab0195823e644af5
U-CORP\dillon.min <dillon.min@CNBNG-LT542.Corp.irdeto.com>
2015-07-22 12:18:02 +0800

    debug flash write,read,erase

--- src/Main/Src/spiflash.c
+++ src/Main/Src/spiflash.c
@@ -155,6 +155,69 @@ int flash_read_reg(unsigned char reg,unsigned char *out,int len)
 	HAL_GPIO_WritePin(SPIx_NSS_GPIO_PORT,SPIx_NSS_PIN, GPIO_PIN_SET);
 	return result;
 }
+int flash_write_reg(unsigned char reg,unsigned char *out,int len)
+{
+	int result=-1;
+	//HAL_SPI_TransmitReceive(&SpiHandle, (uint8_t*)&reg, (uint8_t *)out, len, 5000);
+	HAL_GPIO_WritePin(SPIx_NSS_GPIO_PORT,SPIx_NSS_PIN, GPIO_PIN_RESET);
+	if(out!=NULL)
+	{
+		if(HAL_SPI_Transmit(&SpiHandle,(uint8_t *)&reg,1,5000)==HAL_OK)
+			result=HAL_SPI_Transmit(&SpiHandle,(uint8_t *)out,len,5000);
+	}
+	else
+		result=HAL_SPI_Transmit(&SpiHandle,(uint8_t *)&reg,1,5000);
+	HAL_GPIO_WritePin(SPIx_NSS_GPIO_PORT,SPIx_NSS_PIN, GPIO_PIN_SET);
+	return result;
+}
+void flash_write(int op_code,int addr,int len,int *ret,unsigned char *buf)
+{	
+	unsigned char send_buf[6];
+	int send_len=0;
+	int result=-1;
+	if(op_code==SPINOR_OP_BP)//byte program
+	{
+		send_len=5;
+	}
+	else
+	{
+		send_len=6;
+		send_buf[5]=buf[1];
+	}
+	send_buf[0]=op_code;
+	send_buf[1]=(addr & 0xFFFFFF) >> 16);
+	send_buf[2]=(addr & 0xFFFF) >> 8);
+	send_buf[3]=(addr & 0xFF);
+	send_buf[4]=buf[0];
+	HAL_GPIO_WritePin(SPIx_NSS_GPIO_PORT,SPIx_NSS_PIN, GPIO_PIN_RESET);
+	result=HAL_SPI_Transmit(&SpiHandle,(uint8_t *)send_buf,send_len,5000);
+	HAL_GPIO_WritePin(SPIx_NSS_GPIO_PORT,SPIx_NSS_PIN, GPIO_PIN_SET);
+	if(result==HAL_OK)
+		*ret=len;
+	else
+		*ret=0;
+}
+int flash_read(int addr,int len,int *ret,unsigned char *buf)
+{
+	int result=-1;
+	unsigned char send_buf[5];
+	
+	send_buf[0]=SPINOR_OP_READ_FAST;
+	send_buf[1]=(addr & 0xFFFFFF) >> 16);
+	send_buf[2]=(addr & 0xFFFF) >> 8);
+	send_buf[3]=(addr & 0xFF);
+	send_buf[4]=0xff;
+	HAL_GPIO_WritePin(SPIx_NSS_GPIO_PORT,SPIx_NSS_PIN, GPIO_PIN_RESET);
+	if(HAL_SPI_Transmit(&SpiHandle,(uint8_t *)send_buf,5,5000)==HAL_OK)
+		result=HAL_SPI_Receive(&SpiHandle,(uint8_t *)buf,len,5000);
+	HAL_GPIO_WritePin(SPIx_NSS_GPIO_PORT,SPIx_NSS_PIN, GPIO_PIN_SET);
+	if(result==HAL_OK)
+		*ret=len;
+	else
+		*ret=0;
+	return result;
+
+}
 int spi_nor_read_id()
 {
 	int			tmp;
@@ -187,5 +250,195 @@ int spi_nor_read_id()
 	}
 	printf("unrecognized JEDEC id %06x ext id %x\n", jedec,ext_jedec);
 	return 0;
+}
+#define	MAX_READY_WAIT_JIFFIES	(40 * HZ)
+/*
+ * Read the status register, returning its value in the location
+ * Return the status register value.
+ * Returns negative if error occurred.
+ */
+static int read_sr()
+{
+	int ret;
+	unsigned char val;
+
+	ret = flash_read_reg(SPINOR_OP_RDSR, &val, 1);
+	if (ret < 0) {
+		printf("error %d reading SR\n", (int) ret);
+		return ret;
+	}
+
+	return val;
+}
+/*
+ * Set write enable latch with Write Enable command.
+ * Returns negative if error occurred.
+ */
+static inline int write_enable()
+{
+	return flash_write_reg(SPINOR_OP_WREN, NULL, 0);
+}
+/*
+ * Send write disble instruction to the chip.
+ */
+static inline int write_disable(struct spi_nor *nor)
+{
+	return flash_write_reg(SPINOR_OP_WRDI, NULL, 0);
+}
+
+static int wait_till_ready()
+{
+	unsigned long deadline;
+	int sr;
+	
+	deadline = HAL_GetTick() + MAX_READY_WAIT_JIFFIES;
+
+	do {
+		//cond_resched();
+
+		sr = read_sr();
+		if (sr < 0)
+			break;
+		else if (!(sr & SR_WIP))
+			return 0;
+	} while (HAL_GetTick()<deadline);
+
+	return 1;
+}
+
+static int spi_nor_write(int to, int len,
+		int *retlen, const unsigned char *buf)
+{
+	int actual;
+	int ret,sst_write_second=0;
+	unsigned char	program_opcode;
+	
+	printf("to 0x%08x, len %zd\n", to, len);
+
+	/* Wait until finished previous write command. */
+	ret = wait_till_ready();
+	if (ret)
+		goto time_out;
+
+	write_enable();
+
+	actual = to % 2;
+	/* Start write from odd address. */
+	if (actual) {
+			program_opcode = SPINOR_OP_BP;
+
+		/* write one byte. */
+		flash_write(program_opcode,to, 1, retlen, buf);
+		ret = wait_till_ready();
+		if (ret)
+			goto time_out;
+	}
+	to += actual;
+
+	/* Write out most of the data here. */
+	for (; actual < len - 1; actual += 2) {
+		program_opcode = SPINOR_OP_AAI_WP;
+
+		/* write two bytes. */
+		flash_write(program_opcode, to, 2, retlen, buf + actual);
+		ret = wait_till_ready();
+		if (ret)
+			goto time_out;
+		to += 2;
+		sst_write_second = true;
+	}
+	sst_write_second = false;
+
+	write_disable();
+	ret = wait_till_ready();
+	if (ret)
+		goto time_out;
+
+	/* Write out trailing byte if it exists. */
+	if (actual != len) {
+		write_enable();
+
+		program_opcode = SPINOR_OP_BP;
+		flash_write(program_opcode, to, 1, retlen, buf + actual);
+
+		ret = wait_till_ready();
+		if (ret)
+			goto time_out;
+		write_disable();
+	}
+time_out:
+	return ret;
+}
+static int spi_nor_read(int from, int len,
+			int *retlen, unsigned char *buf)
+{
+	int ret;
+
+	printf("from 0x%08x, len %zd\n", from, len);
+
+	ret = flash_read(from, len, retlen, buf);
+	return ret;
+}
+/*
+ * Erase an address range on the nor chip.  The address range may extend
+ * one or more erase sectors.  Return an error is there is a problem erasing.
+ */
+static int spi_nor_erase(struct mtd_info *mtd, struct erase_info *instr)
+{
+	struct spi_nor *nor = mtd_to_spi_nor(mtd);
+	u32 addr, len;
+	uint32_t rem;
+	int ret;
+
+	dev_dbg(nor->dev, "at 0x%llx, len %lld\n", (long long)instr->addr,
+			(long long)instr->len);
+
+	div_u64_rem(instr->len, mtd->erasesize, &rem);
+	if (rem)
+		return -EINVAL;
+
+	addr = instr->addr;
+	len = instr->len;
+
+	ret = spi_nor_lock_and_prep(nor, SPI_NOR_OPS_ERASE);
+	if (ret)
+		return ret;
+
+	/* whole-chip erase? */
+	if (len == mtd->size) {
+		if (erase_chip(nor)) {
+			ret = -EIO;
+			goto erase_err;
+		}
+
+	/* REVISIT in some cases we could speed up erasing large regions
+	 * by using SPINOR_OP_SE instead of SPINOR_OP_BE_4K.  We may have set up
+	 * to use "small sector erase", but that's not always optimal.
+	 */
+
+	/* "sector"-at-a-time erase */
+	} else {
+		while (len) {
+			if (nor->erase(nor, addr)) {
+				ret = -EIO;
+				goto erase_err;
+			}
+
+			addr += mtd->erasesize;
+			len -= mtd->erasesize;
+		}
+	}
+
+	spi_nor_unlock_and_unprep(nor, SPI_NOR_OPS_ERASE);
+
+	instr->state = MTD_ERASE_DONE;
+	mtd_erase_callback(instr);
+
+	return ret;
+
+erase_err:
+	spi_nor_unlock_and_unprep(nor, SPI_NOR_OPS_ERASE);
+	instr->state = MTD_ERASE_FAILED;
+	return ret;
 }
 
